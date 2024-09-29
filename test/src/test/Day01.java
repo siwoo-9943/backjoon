24eb73f839fc4280b8a1212f607312b3441075ea
siwoo-9943 <siwoo9943@gmail.com>
2024-09-29 23:30:03 +0900

    Day01

---
+++ test/src/test/Day01.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    System.out.println(a+b);
+	}
+}
