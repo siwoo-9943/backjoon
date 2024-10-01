0c6ad3879576cf67713f4d29e77c73ef01e9a570
siwoo-9943 <siwoo9943@gmail.com>
2024-10-01 23:55:21 +0900

    day03

---
+++ test/src/day03/Task02.java
@@ -0,0 +1,17 @@
+package day03;
+
+import java.util.Scanner;
+
+public class Task02 {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A*B);
+ 
+		in.close();
+	}
+}
--- test/src/test/Day02.java
+++ test/src/test/Day02.java
@@ -1,5 +1,21 @@
 package test;
 
-public class Day02 {
+import java.util.Scanner;
 
-}
+public class Day02 {
+		 
+		public static void main(String[] args) {
+	 
+			Scanner in = new Scanner(System.in);
+			int A = in.nextInt();
+			int B = in.nextInt();
+			
+			
+			
+			System.out.println(A-B);
+	 
+			in.close();
+		}
+	}
+
+
